8b64fb3435bf12bba3d3e6689711867f26fc8b2e
DENIS OH <98143219+Denis-Oh@users.noreply.github.com>
2024-04-12 23:01:21 -0400

    Banner Section Complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import './App.css';
+import CoverArt from './assets/LOVE.jpeg';
 
 function App() {
   return (
     <div className="app">
-      <header className="banner">Banner Section</header>
+      <header className="banner">
+        <nav className="navigation">
+          <h2>BeatBabble</h2>
+          <h4>My Songs</h4>
+          <h4>Discover</h4>
+          <h4>Languages</h4>
+        </nav>
+        <div className="song-title">
+          <img src={CoverArt} alt="CoverArt"></img>
+          <div className="song-title-info">
+            <h2>L-O-V-E</h2>
+            <h4>Nat King Cole</h4>
+          </div>
+        </div>
+      </header>
       <div className="content">
         <div className="left-section">Left Section</div>
         <div className="right-section">Right Section</div>

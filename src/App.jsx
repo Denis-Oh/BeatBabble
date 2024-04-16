7f8c81027351c75542cd0f43707b8811434f63dc
ZTheGod <108885304+ZTheGod@users.noreply.github.com>
2024-04-16 01:27:55 -0400

    In progress changes and added discord sound effect

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import React, { useState } from 'react';
 import './App.css';
 import CoverArt from './assets/LOVE.jpeg';
 import WordPopup from './WordPopup';
+import useSound from 'use-sound'
+import mySound from './assets/discord_ping_sound_effect.mp3' // Your sound file path here
+import { Wave } from 'react-animated-text'
 
 const lyricsEnglish = "L is for the way you look at me %NL O is for the only one I see %NL V is very, very extraordinary %NL E is even more than anyone that you adore can  %NL%NL Love is all that I can give to you %NL Love is more than just a game for two %NL Two in love can make it %NL Take my heart and please don't break it %NL Love was made for me and you";
 const lyricsFrench = "L est pour la façon dont tu me regardes %NL O est pour la seule personne que je vois %NL V est pour vraiment, vraiment extraordinaire %NL E est encore plus que quiconque que tu puisses adorer %NL%NL L'amour est tout ce que je peux te donner %NL L'amour est plus qu'un simple jeu à deux %NL Deux en amour peuvent y arriver %NL Prends mon coeur et s'il te plaît ne le brise pas %NL L'amour est fait pour moi et toi";
 // Split lyrics into parts by '%NL', then map to words and inject <br /> elements for line breaks
-const wordsWithPunctuation = lyricsEnglish.split('%NL').flatMap((line, index, array) => {
+const wordsWithPunctuation = lyricsFrench.split('%NL').flatMap((line, index, array) => {
   const words = line.match(/[\w'-]+|[.,!?;]/g) || [];
   const elements = words.map((word, idx) => ({
     word,
@@ -18,6 +21,14 @@ const wordsWithPunctuation = lyricsEnglish.split('%NL').flatMap((line, index, ar
   }
   return elements;
 });
+
+//Awful attempt for formatting the left side
+const temp = lyricsEnglish.split('%NL')
+var temp2 = ""
+for (let i = 0; i < temp.length; i++) {
+  temp2 += temp[i] + "\n";
+}
+
 
 function App() {
   const [selectedWord, setSelectedWord] = useState(null);
@@ -33,16 +44,22 @@ function App() {
           <h4>Découvrir</h4>
           <h4>Langues</h4>
         </nav>
-        <div className="song-title">
-          <img src={CoverArt} alt="CoverArt"></img>
+          <div className="song-title">
+            <img src={CoverArt} alt="CoverArt"></img>
           <div className="song-title-info">
             <h2>L-O-V-E</h2>
             <h4>Nat King Cole</h4>
           </div>
+          <SoundPlayer />
         </div>
       </header>
       <div className="content">
         <div className="left-section">
+          <div>
+            <Wave2 />
+          </div>
+        </div>
+        <div className="right-section">
           <p>
             {wordsWithPunctuation.map((element, index) => {
               if (React.isValidElement(element)) { // Render <br /> as is
@@ -62,14 +79,60 @@ function App() {
           </p>
           {selectedWord && <WordPopup word={selectedWord} closePopup={closePopup} />}
         </div>
-        <div className="right-section">
-          <p>
-          L est pour la façon dont tu me regardes <br /> O est pour la seule personne que je vois <br /> V est pour vraiment, vraiment extraordinaire <br /> E est encore plus que quiconque que tu puisses adorer <br /><br /> L'amour est tout ce que je peux te donner <br /> L'amour est plus qu'un simple jeu à deux <br /> Deux en amour peuvent y arriver <br /> Prends mon coeur et s'il te plaît ne le brise pas <br /> L'amour est fait pour moi et toi"
-          </p>
-        </div>
       </div>
     </div>
   );
+}
+
+//new function, It will play the song but it's currently set to the discord ping sound
+function SoundPlayer() {
+  const [playSound] = useSound(mySound)
+  
+  return (
+    <button onClick={() => {
+      playSound();
+      }}>
+       <Wave text="Play song!" effect="jump" effectChange={0.5} />
+    </button>
+  )
+}
+
+//This is responsible for creating the colored text
+export class Wave2 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { paused: true };
+    this.togglePause = this.togglePause.bind(this);
+    this.reset = this.reset.bind(this);
+  }
+
+  togglePause() {
+    this.setState(prevState => ({ paused: !prevState.paused }));
+  }
+
+  reset() {
+    this.setState('paused', true);
+  }
+
+  render() { //This is where all the magic is, It's currently set to start on click of both the text and button
+    return (
+      <div>
+        <a onClick={this.togglePause} style={{cursor: 'pointer'}}>
+          <Wave
+            text={temp2}
+            effect="color"
+            effectChange="red"
+            effectDuration={1.5}
+            iterations={1}
+            paused={this.state.paused}
+          />
+        </a>
+        <br />
+        <button onClick={this.togglePause}>Reset</button>
+
+      </div>
+    )
+  }
 }
 
 export default App;

0af7fb1dff7bd1a7ce9d5e5a28f55661a5b79e74
ZTheGod <108885304+ZTheGod@users.noreply.github.com>
2024-04-16 21:01:45 -0400

    Added a working word scroll and music when lyrics are clicked

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import './App.css';
 import CoverArt from './assets/LOVE.jpeg';
 import WordPopup from './WordPopup';
 import useSound from 'use-sound'
-import mySound from './assets/discord_ping_sound_effect.mp3' // Your sound file path here
+import mySound from './assets/LOVE.mp3' // Your sound file path here
 import { Wave } from 'react-animated-text'
 
-const lyricsEnglish = "L is for the way you look at me %NL O is for the only one I see %NL V is very, very extraordinary %NL E is even more than anyone that you adore can  %NL%NL Love is all that I can give to you %NL Love is more than just a game for two %NL Two in love can make it %NL Take my heart and please don't break it %NL Love was made for me and you";
-const lyricsFrench = "L est pour la façon dont tu me regardes %NL O est pour la seule personne que je vois %NL V est pour vraiment, vraiment extraordinaire %NL E est encore plus que quiconque que tu puisses adorer %NL%NL L'amour est tout ce que je peux te donner %NL L'amour est plus qu'un simple jeu à deux %NL Deux en amour peuvent y arriver %NL Prends mon coeur et s'il te plaît ne le brise pas %NL L'amour est fait pour moi et toi";
+const lyricsEnglish = "L is for the way you look at me                       O is for the only one I see                            V is very, very extraordinary                           E is even more than anyone that you adore can       Love is all that I can give to you                      Love is more than just a game for two                Two in love can make it                               Take my heart and please don't break it               Love was made for me and you";
+const lyricsFrench = "L est pour la façon dont tu me regardes %NL O est pour la seule personne que je vois %NL V est pour vraiment, vraiment extraordinaire %NL E est encore plus que quiconque que tu puisses adorer %NL L'amour est tout ce que je peux te donner %NL L'amour est plus qu'un simple jeu à deux %NL Deux en amour peuvent y arriver %NL Prends mon coeur et s'il te plaît ne le brise pas %NL L'amour est fait pour moi et toi";
 // Split lyrics into parts by '%NL', then map to words and inject <br /> elements for line breaks
 const wordsWithPunctuation = lyricsFrench.split('%NL').flatMap((line, index, array) => {
   const words = line.match(/[\w'-]+|[.,!?;]/g) || [];
@@ -21,13 +21,6 @@ const wordsWithPunctuation = lyricsFrench.split('%NL').flatMap((line, index, arr
   }
   return elements;
 });
-
-//Awful attempt for formatting the left side
-const temp = lyricsEnglish.split('%NL')
-var temp2 = ""
-for (let i = 0; i < temp.length; i++) {
-  temp2 += temp[i] + "\n";
-}
 
 
 function App() {
@@ -50,14 +43,13 @@ function App() {
             <h2>L-O-V-E</h2>
             <h4>Nat King Cole</h4>
           </div>
-          <SoundPlayer />
         </div>
       </header>
       <div className="content">
         <div className="left-section">
-          <div>
-            <Wave2 />
-          </div>
+          <span>
+            <SoundPlayer />
+          </span>
         </div>
         <div className="right-section">
           <p>
@@ -84,55 +76,42 @@ function App() {
   );
 }
 
-//new function, It will play the song but it's currently set to the discord ping sound
 function SoundPlayer() {
   const [playSound] = useSound(mySound)
-  
+  const [index, setIndex] = useState(0);
+  const lyrics = [<Test paused={true} />, <Test paused={false} />]
+
+  function handleClick() {
+    if (index == 0){
+      setIndex(index + 1);
+    } else{
+      setIndex(index - 1);
+    }
+  }
+
   return (
-    <button onClick={() => {
-      playSound();
-      }}>
-       <Wave text="Play song!" effect="jump" effectChange={0.5} />
-    </button>
+    <span onClick={() => {handleClick(); playSound()}}>
+      {lyrics[index]}
+    </span>
   )
 }
 
-//This is responsible for creating the colored text
-export class Wave2 extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { paused: true };
-    this.togglePause = this.togglePause.bind(this);
-    this.reset = this.reset.bind(this);
-  }
-
-  togglePause() {
-    this.setState(prevState => ({ paused: !prevState.paused }));
-  }
-
-  reset() {
-    this.setState('paused', true);
-  }
-
-  render() { //This is where all the magic is, It's currently set to start on click of both the text and button
-    return (
-      <div>
-        <a onClick={this.togglePause} style={{cursor: 'pointer'}}>
-          <Wave
-            text={temp2}
-            effect="color"
-            effectChange="red"
-            effectDuration={1.5}
-            iterations={1}
-            paused={this.state.paused}
-          />
-        </a>
-        <br />
-        <button onClick={this.togglePause}>Reset</button>
-
-      </div>
-    )
-  }
+function Test(props) {
+  return(
+    <span style={{cursor: 'pointer'}}>
+      {<Wave
+        text={lyricsEnglish}
+        effect="color"
+        speed={8.7}
+        effectChange="red"
+        effectDuration={2.5}
+        iterations={1}
+        paused={props.paused}
+      />}
+      <br />
+    </span>
+  )
 }
 
 export default App;
+
